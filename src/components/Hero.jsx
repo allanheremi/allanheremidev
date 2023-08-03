5cd8cd2cdd51e18bce7f72fc5e2ce16749a53839
Allan Heremi <allanheremidev@gmail.com>
2023-08-03 12:54:48 +0200

    Feat: WIP Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,13 @@ const Hero = () => {
         <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
         <div className='w-1 sm:h-80 h-40 violet-gradient'/>
       </div>
-      <div><h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Allan</span></h1></div>
+      <div><h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Allan</span></h1>
+      <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+        I develop smart contracts, 3D visual user <br className='sm:block hidden' /> interfaces and web applications.
+        </p>
+        </div>
     </div>
+        <ComputersCanvas />
   </section>;
 };
 
