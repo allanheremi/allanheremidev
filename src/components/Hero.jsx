6529e01aa59884e18d4d0e54b588703193375b82
Allan Heremi <allanheremidev@gmail.com>
2023-08-03 18:58:56 +0200

    Minor fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,17 +8,17 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 lg:top-[60px]`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
+          <h1 className={`${styles.heroHeadText} text-white lg:text-[36px]`}>
             Hi, I'm <span className="text-[#915eff]">Allan</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          <p className={`${styles.heroSubText} mt-2 text-white-100 lg:text-[24px]`}>
             I develop smart contracts, 3D visuals,{' '}
             <br className="sm:block hidden" /> and web applications.
           </p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav
       className={`
-    ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary
+    ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary lg:py-4
     `}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
@@ -25,7 +25,7 @@ const Navbar = () => {
           }}
         >
 
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={'src/assets/allan.png'} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             
             Allan &nbsp;
