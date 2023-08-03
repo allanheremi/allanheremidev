adcfcffb733423e61ae4b9296c7a6644682b9f56
Allan Heremi <allanheremidev@gmail.com>
2023-08-03 13:39:21 +0200

    Feat: model displaying

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,20 +5,36 @@ import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
-  return <section className='relative w-full h-screen mx-auto'>
-    <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-      <div className='flex flex-col justify-center items-center mt-5'>
-        <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-        <div className='w-1 sm:h-80 h-40 violet-gradient'/>
-      </div>
-      <div><h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Allan</span></h1>
-      <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-        I develop smart contracts, 3D visual user <br className='sm:block hidden' /> interfaces and web applications.
-        </p>
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
-    </div>
-        <ComputersCanvas />
-  </section>;
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Allan</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop smart contracts, 3D visuals,{' '}
+            <br className="sm:block hidden" /> and web applications.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className='w-[36px] h-[64px] rounded-3xl border-4 border-gray-300 flex justify-center items-start p-2'>
+            <motion.dev animate={{y: [0, 24, 0]}} transition={{duration: 1.5, repeat: Infinity, repeatType: 'loop'}} className='w-2 h-2 rounded-full bg-gray-300'/>
+          </div>
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
