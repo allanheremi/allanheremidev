09d35456733864d6eea1806c1b75356de3b90689
Allan Heremi <allanheremidev@gmail.com>
2023-08-03 19:50:09 +0200

    Fix: redundant imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,6 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 import enics from './company/enics.png'
 import ethereum from './company/ethereum.png'
 import salt from './company/salt.png'
@@ -55,10 +51,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
