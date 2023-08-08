96ed9b631351c35001c295276b8ccbba9ded3dca
Allan Heremi <allanheremidev@gmail.com>
2023-08-08 22:04:16 +0200

    Fix: remove fallback bg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": ["url('/src/assets/testherobg.jpeg')", "url('/src/assets/herobg.png')"],
+        "hero-pattern": "url('/src/assets/testherobg.jpeg')",
       },
       linearGradientColors: {
         'transparent-to-primary': ['transparent', '#2a2a2a'],
