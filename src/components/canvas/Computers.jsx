b402379cc8b330128cf4a1a1256e39c924e5110f
Allan Heremi <allanheremidev@gmail.com>
2023-08-08 22:07:51 +0200

    Fix: desktop 3D size

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,8 +21,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.56 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.57 : 0.75}
+        position={isMobile ? [-4, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -53,7 +53,7 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [15, 15, 18], fov: 33 }}
+      camera={{ position: [20, 3, 5], fov: 33 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
