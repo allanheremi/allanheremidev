1f70c9458ba37623fb8bc68645a217fa5909b70a
Allan Heremi <allanheremidev@gmail.com>
2023-08-08 22:12:21 +0200

    Fix: sizing

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,7 +21,7 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.57 : 0.75}
+        scale={isMobile ? 0.7 : 0.92}
         position={isMobile ? [-4, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -53,7 +53,7 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 33 }}
+      camera={{ position: [25, 3, 5], fov: 33 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
