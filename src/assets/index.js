18a6936a5c4852f5f94f5566fde090885c0bd1c6
Allan Heremi <allanheremidev@gmail.com>
2023-10-06 11:21:55 +0200

    Fix: redundant lines removed

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,37 +1,34 @@
-import allan from "./allan.png";
-import backend from "./backend.png";
-import blockchain from "./blockchain.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
-import github from "./github.png";
-import menu from "./menu.svg";
-import close from "./close.svg";
-
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import solidity from './tech/solidity.png'
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
-
-import enics from './company/enics.png'
-import ethereum from './company/ethereum.png'
-import salt from './company/salt.png'
-
-import githubpreview from './githubpreview.png'
-import leetcode from './leetcode.png'
-import leetcodelogo from './leetcodelogo.png'
-import threemesh from './threemesh.png'
-import web3 from './web3.png'
-import lightblock from './lightblock.png'
+import allan from './allan.png';
+import backend from './backend.png';
+import blockchain from './blockchain.png';
+import mobile from './mobile.png';
+import web from './web.png';
+import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import solidity from './tech/solidity.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import mongodb from './tech/mongodb.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import threejs from './tech/threejs.svg';
+import enics from './company/enics.png';
+import ethereum from './company/ethereum.png';
+import salt from './company/salt.png';
+import githubpreview from './githubpreview.png';
+import leetcode from './leetcode.png';
+import leetcodelogo from './leetcodelogo.png';
+import threemesh from './threemesh.png';
+import web3 from './web3.png';
+import lightblock from './lightblock.png';
 
 export {
   lightblock,
@@ -61,8 +58,8 @@ export {
   typescript,
   threejs,
   enics,
-  ethereum, 
+  ethereum,
   salt,
   web3,
-  threemesh
+  threemesh,
 };
