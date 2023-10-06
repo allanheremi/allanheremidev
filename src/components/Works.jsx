216b2c26b2453bf6659a30698df5999faed37a1e
Allan Heremi <allanheremidev@gmail.com>
2023-10-06 13:55:57 +0200

    Style: decrease font size

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -53,14 +53,14 @@ const ProjectCard = ({
 
         <div className='mt-5'>
           <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-white-100 text-[14px]'>{description}</p>
+          <p className='mt-2 text-white-100 text-[12px]'>{description}</p>
         </div>
 
         <div className='mt-4 flex flex-wrap gap-2'>
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
+              className={`text-[12px] ${tag.color}`}
             >
               #{tag.name}
             </p>
